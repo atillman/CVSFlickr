c00d2eec70c41671b8ef276df0ca9a8c9e3899a6
Andrew Tillman <andrewtillman@Andrews-MBP-M2.lan>
2023-11-26 17:29:15 -0600

    Formatting

--- FlickrBrowser/FlickrBrowser/FlickrBrowserApp.swift
+++ FlickrBrowser/FlickrBrowser/FlickrBrowserApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct FlickrBrowserApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
